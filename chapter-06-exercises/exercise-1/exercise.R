739327e05f4612d0c62a63fe95ec055ce1e9a7c0
Dhandeep Suglani <dhandeep@Dhandeeps-MacBook-Air.local>
2020-01-17 01:06:18 -0800

    Week 1 Exercises done

--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -26,22 +26,25 @@ fav_2 <- 25
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
-
+fav_1 <- fav_1 / sqrt(201)
+fav_2 <- fav_2 / sqrt(201)
 
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
-
+raw_sum <- sum(fav_1, fav_2)
 
 # Create a variable `round_sum` that is the `raw_sum` rounded to 1 decimal place.
 # Use the `round()` function.
-
+round_sum <- round(raw_sum)
 
 # Create two new variables `round_1` and `round_2` that are your `fav_1` and 
 # `fav_2` variables rounded to 1 decimal places
-
+round_1 <- round(fav_1)
+round_2 <- round(fav_2)
 
 # Create a variable `sum_round` that is the sum of the rounded values
-
+sum_round <- sum(round_1, round_2)
 
 # Which is bigger, `round_sum` or `sum_round`? (You can use the `max()` function!)
+max(round_sum, sum_round)
 
--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -2,44 +2,50 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- c(10:20)
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next_ten <- c(21:30)
 
 # Create a vector `all_numbers` by combining the previous two vectors
+all_numbers <- c(first_ten + next_ten)
 
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
+eleventh <- all_numbers[11]
 
 
 # Create a vector `some_numbers` that contains the 2nd through the 5th elements 
 # of `all_numbers`
-
+some_numbers <- all_numbers[2:5]
 
 
 # Create a vector `even` that holds the even numbers from 1 to 100
+hundred <- c(1:100)
+if (evenly <- hundred / 2 == 0
+even <- hundred[evenly]
 
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-
+all(even%2)
 
 
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
-
+phone_numbers <- c(8,6,7,5,3,0,9)
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
+prefix <-c(phone_numbers[1:3])
 
 
 # Create a vector `small` that has the values of `phone_numbers` that are 
 # less than or equal to 5
-
+small <- c(phone_numbers <= 5)
 
 # Create a vector `large` that has the values of `phone_numbers` that are 
 # strictly greater than 5
-
+large <- c(phone_numbers => 5)
 
 # Replace the values in `phone_numbers` that are larger than 5 with the number 5
 
